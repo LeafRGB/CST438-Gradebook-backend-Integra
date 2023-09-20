242f3cd880a54f119787274c0f20bffc0c8fe937
Integra <josed21st@gmail.com>
2023-09-19 20:46:22 -0700

    Assignment Controller Updated, no Junit tests

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,5 +1,6 @@
 package com.cst438.controllers;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,4 +52,84 @@ public class AssignmentController {
 	}
 	
 	// TODO create CRUD methods for Assignment
+	
+	//get by primary key
+	@GetMapping("/assignment/getByID/{id}")
+	public AssignmentDTO[] getCourse(@PathVariable("id") int id) {
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+		AssignmentDTO[] result = new AssignmentDTO[1];
+		for (int i=0; i<assignments.size(); i++) {
+			Assignment as = assignments.get(i);
+			if(as.getId() == id) {
+				AssignmentDTO dto = new AssignmentDTO(
+						as.getId(), 
+						as.getName(), 
+						as.getDueDate().toString(), 
+						as.getCourse().getTitle(), 
+						as.getCourse().getCourse_id());
+				result[0]=dto;
+				break;
+			}
+			
+		}
+		return result;
+	}
+	// delete a course
+	//  DELETE  /course/12389
+	//  DELETE  /course/12389?force=yes
+	@DeleteMapping("/assignment/delete/{id}")
+	public void deleteCourse(@PathVariable("id") int id) {
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+		for (int i=0; i<assignments.size(); i++) {
+			Assignment as = assignments.get(i);
+			if(as.getId() == id) {
+				assignmentRepository.delete(as);
+			}
+			
+		}
+		// @RequestParam("force") Optional<String> force)
+	}
+	
+	@PutMapping("/assignment/update/{id}")
+	public void updateAssignment(@RequestBody AssignmentDTO assignmentDTO, @PathVariable("id") int id) {
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+		for (int i=0; i<assignments.size(); i++) {
+			Assignment as = assignments.get(i);
+			if(as.getId() == i) {
+				as.setDueDate(Date.valueOf(assignmentDTO.dueDate()));
+		        as.setName(assignmentDTO.assignmentName());
+		        Course course = courseRepository.findById(assignmentDTO.courseId()).orElse(null);
+		        if(course != null) {
+		        	as.setCourse(course);
+		        }
+		        assignmentRepository.save(as);
+		        break;
+			}
+			
+		}
+	}
+
+	@PutMapping("/assignment/new/")
+	public void createAssignment(@RequestBody AssignmentDTO assignmentDTO, int id, String name, Date date, int courseID) {
+		//String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		//List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+		Course course = courseRepository.findById(assignmentDTO.courseId()).orElse(null);
+		String courseName = "";
+		
+        if(course != null) {
+        	courseName = course.getTitle();
+        }
+		
+		Assignment newAssignment = new Assignment();
+		newAssignment.setCourse(course);
+		newAssignment.setDueDate(date);
+		newAssignment.setId(id);
+		newAssignment.setName(courseName);
+		assignmentRepository.save(newAssignment);
+		
+	}
+	                       
 }
